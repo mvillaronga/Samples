17dd3e0f8ba653171af48b289b006385ef1fd767
Unknown <michael.villaronga@gmail.com>
2018-03-26 23:56:12 -0500

    App Created

---
+++ src/components/dropdown/Dropdown.js
@@ -0,0 +1,14 @@
+import  React, { Component } from 'react';
+//import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+
+class Dropdown extends Component {
+    render() {
+        return (
+                <p>Hi there</p>
+        );
+    }
+}
+
+//export default connect(null, null)(Home);
+export Dropdown;
